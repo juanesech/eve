fb0b265b4054cdcaaf8b47f7531a357fd8204ce0
Juanesech <juanesech@gmail.com>
2023-07-30 00:40:36 +0200

    ci(cicada):update

--- .cicada/eve.ts
+++ .cicada/eve.ts
@@ -17,36 +17,13 @@ const build = new Job({
   ],
 });
 
-const test = new Job({
-  name: "My First Job",
-  image: "ubuntu:22.04",
-  steps: [
-    {
-      name: "Run bash",
-      run: "echo hello from bash!",
-    },
-    {
-      name: "Run deno/typescript",
-      run: () => {
-        console.log("Hello from deno typescript");
-        console.log("I can read the local file system");
-        console.log("e.g. here are all the directories in my project");
-        console.log(Array.from(Deno.readDirSync("/app")).map((d) => d.name));
-        console.log("Or I can see my environment variables");
-        console.log(Deno.env.toObject());
-      },
-    },
-  ],
-});
-
 export default new Pipeline(
   [
     build,
-    test
   ],
   {
     on: {
-      pullRequest: ["main"],
+      pullRequest: ["all"],
       push: ["*"],
     },
   },
